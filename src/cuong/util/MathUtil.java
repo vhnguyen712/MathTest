1eee4063206b1ef1ce96c8afad163afaf936b8af
Cuong <cuong.tran.vt19@gmail.com>
2020-07-22 09:55:55 +0700

    refactor the cF() method by using the recursion technic

--- src/cuong/util/MathUtil.java
+++ src/cuong/util/MathUtil.java
@@ -22,10 +22,11 @@ public class MathUtil {
         if(n <0 || n > 15){
             throw new IllegalArgumentException("Invalid argument. n must be >= 0 and <=15");
         }
-        long result = 1;
-        for (int i = 1; i <= n; i++) {
-            result *= i;
-        }
-        return result;
+        if(n == 0 || n == 1)
+            return 1;   //điểm dừng của đệ quy
+        //sống sót đến dưới đây, sure, n 2...15
+        //ta ko xài else làm gì cả
+        return n * computeFactorial(n - 1); //n! = n * (n - 1)!
+        
     }
 }
