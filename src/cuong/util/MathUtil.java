8363b5a6cf7721180dd03de4e2f001d4a82ca2bb
vhnguyen712 <53395354+vhnguyen712@users.noreply.github.com>
2021-03-17 15:18:29 +0700

    Update MathUtil.java

--- src/cuong/util/MathUtil.java
+++ src/cuong/util/MathUtil.java
@@ -17,7 +17,7 @@ public class MathUtil {
     //vì giá trị giai thừa tăng cực nhanh, nên sẽ tràn vùng int rất sớm
     //do int chỉ tối đa 2 tỷ 1 nên ta xài long thì mới chứa đc n lớn
     //nhưng cũng chỉ cỡ 15! thoy.
-    public static long computeFactorial(int n) {
+    //public static long computeFactorial(int n) {
         
         if(n <0 || n > 15){
             throw new IllegalArgumentException("Invalid argument. n must be >= 0 and <=15");
